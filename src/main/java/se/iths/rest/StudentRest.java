f5d9cc710c1d5bda06e82a4c83a0aba37b2ab20c
Sweet4ever <oskpil@gmail.com>
2021-11-17 12:38:32 +0100

    kod som inte funkar som väntat

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,45 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("student")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("new")
+    @POST
+    public Response createStudent(Student student){
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("update")
+    @PUT
+    public Response updateStudent(Student student){
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Student getStudent(@PathParam("id") Long id){
+        return studentService.findStudentById(id);
+    }
+
+    @Path("all")
+    @GET
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
 }
